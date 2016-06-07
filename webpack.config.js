af0b64bb2df6a798a2630dc1bae4dd28ba1f7859
Alvin Chan <aschan@gmail.com>
2016-06-07 09:48:04 -0700

    Move Babel options into Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var path = require("path"),
+const path = require("path"),
   webpack = require("webpack");
 
-var src = path.join(__dirname, "src"),
+const src = path.join(__dirname, "src"),
   dest = path.join(__dirname, "public"),
   test = path.join(__dirname, "test");
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: src,
@@ -34,6 +34,9 @@ module.exports = {
 
     loaders: [{
       loader: "babel",
+      query: {
+        presets: ["es2015"]
+      },
       test: /\.tag$|\.js$/,
       include: [src, test]
     }, {
