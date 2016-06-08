c42f3a2da84b72e4ef2f5f47590e71dfa0e6705b
Alvin Chan <aschan@gmail.com>
2016-06-07 17:03:58 -0700

    Move Webpack's testing config into `test/`

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,4 +1,4 @@
-const config = require("./webpack.config.js"),
+const config = require("../webpack.config.js"),
   nodeExternals = require("webpack-node-externals");
 
 config.externals = [nodeExternals()];
