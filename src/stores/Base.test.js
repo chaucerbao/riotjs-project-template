411160f7ea35e634fd4d7a1300f787571ac9dcb1
Alvin Chan <aschan@gmail.com>
2017-02-21 11:52:34 -0800

    Test that BaseStore's _fetch() throws on repeated call during a pending request

--- src/stores/Base.test.js
+++ src/stores/Base.test.js
@@ -6,27 +6,40 @@ import fetch from 'node-fetch';
 // Test subject
 import { BaseStore, BaseModel } from './Base';
 
-test('_load()', t => {
-  const store = new BaseStore();
+let store;
 
+// Setup and teardown
+test.beforeEach(t => {
+  store = new BaseStore(fetch);
+});
+
+test.afterEach(t => {
+  nock.cleanAll();
+});
+
+test.after(t => {
+  nock.restore();
+});
+
+// Tests
+test('_load() returns the same instances from cache', t => {
   // Cache is empty
   t.is(store._cache[5], undefined);
   t.is(store._cache[6], undefined);
 
-  // Create two different instance
+  // Create two different instances
   const instanceA = store._load(5);
   const instanceB = store._load(6);
   t.not(store._cache[5], undefined);
   t.not(store._cache[6], undefined);
   t.not(instanceA, instanceB);
 
-  // Cache returns the exact instance
+  // Cache returns the exact instances
   t.is(store._cache[5], instanceA);
   t.is(store._cache[6], instanceB);
 });
 
-test('_fetch()', async t => {
-  const store = new BaseStore(fetch);
+test('_fetch() makes an HTTP call and returns the response', async t => {
   const http = nock('http://example.com').get('/').reply(200, {
     id: 8,
     name: 'Sample response'
@@ -41,10 +54,10 @@ test('_fetch()', async t => {
   t.deepEqual(responseBody, { id: 8, name: 'Sample response' });
 });
 
-test.afterEach(t => {
-  nock.cleanAll();
-});
+test('_fetch() throws when called again during a pending request', async t => {
+  const http = nock('http://example.com').get('/').reply(200, {}).persist();
 
-test.after(t => {
-  nock.restore();
+  store._fetch('http://example.com/');
+
+  const err = await t.throws(store._fetch('http://example.com/'));
 });
