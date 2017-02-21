ae1f01729c4d21809d9da5024ca6f112057d788f
Alvin Chan <aschan@gmail.com>
2017-02-21 12:11:04 -0800

    Test that BaseModel's _update() only sets existing properties

--- src/stores/Base.test.js
+++ src/stores/Base.test.js
@@ -61,3 +61,34 @@ test('_fetch() throws when called again during a pending request', async t => {
 
   const err = await t.throws(store._fetch('http://example.com/'));
 });
+
+test('_update() only updates existing properties', t => {
+  // Extend BaseModel to define properties
+  class Model extends BaseModel {
+    constructor() {
+      super();
+      this.a = [];
+      this.c = '';
+      this.e = 0;
+    }
+  }
+
+  // Create an instance
+  const model = new Model();
+
+  // Update the model's properties
+  model._update({
+    a: [7, 9],
+    b: true,
+    c: 'Text',
+    d: 18,
+    e: { n: 1, m: 6 }
+  });
+
+  // Only existing properties are set
+  t.deepEqual(model.a, [7, 9]);
+  t.is(model.b, undefined);
+  t.is(model.c, 'Text');
+  t.is(model.d, undefined);
+  t.deepEqual(model.e, { n: 1, m: 6 });
+});
